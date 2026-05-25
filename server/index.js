191912d03ec5de9719a4929f17a3c684936cb1d0
fitsum-MGM <maryefitsum@gmail.com>
2026-05-25 12:29:26 +0300

    Phase 2: MongoDB connected successfully

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const cors = require('cors')
+require('dotenv').config()
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch((err) => console.log('MongoDB connection error:', err))
 
 app.get('/', (req, res) => {
   res.json({ message: 'Server is running!' })
