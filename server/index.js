df26c3fc6f12d39f33aa0771400f6c9a61f39b92
fitsum-MGM <maryefitsum@gmail.com>
2026-07-03 12:27:49 +0300

    Fix: allow all CORS origins for production

--- server/index.js
+++ server/index.js
@@ -30,6 +30,19 @@ const io = new Server(server, {
 })
 
 // Middleware
+app.use((req, res, next) => {
+  const origin = req.headers.origin || '*'
+  res.setHeader('Access-Control-Allow-Origin', origin)
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  res.setHeader('Vary', 'Origin')
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204)
+  }
+
+  next()
+})
 app.use(cors(corsOptions))
 app.use(express.json())
 
