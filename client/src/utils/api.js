18e464c4b0e35f6f43fc2a2a3fe8fd103397ddc3
fitsum-MGM <maryefitsum@gmail.com>
2026-07-03 11:26:56 +0300

    Phase 7: add production environment config

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -4,7 +4,6 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
 })
 
-// Automatically add token to every request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
   if (token) {
@@ -13,7 +12,6 @@ api.interceptors.request.use((config) => {
   return config
 })
 
-// Handle expired token
 api.interceptors.response.use(
   (response) => response,
   (error) => {
